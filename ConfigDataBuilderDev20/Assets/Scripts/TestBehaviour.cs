b2b000df8f40111e58b74b6c62a90178aff4d4f5
Kotono <tadanotorch@gmail.com>
2022-02-07 16:29:31 +0800

    Update to 0.1.4

--- ConfigDataBuilderDev20/Assets/Scripts/TestBehaviour.cs
+++ ConfigDataBuilderDev20/Assets/Scripts/TestBehaviour.cs
@@ -1,17 +1,69 @@
+using System;
 using ConfigDataDev;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace ConfigDataBuilderDev
 {
     public class TestBehaviour : MonoBehaviour
     {
-        // Start is called before the first frame update
-        private void Start()
+        private readonly List<(string name, Action action)> _list;
+
+        private TestBehaviour()
+        {
+            _list = new List<(string, Action)>();
+            foreach (var method in typeof(TestBehaviour).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)) {
+                var attr = method.GetCustomAttribute<DebugMenuAttribute>();
+                if (attr != null) {
+                    _list.Add((attr.Name ?? method.Name, (Action)method.CreateDelegate(typeof(Action), this)));
+                }
+            }
+        }
+
+        private class DebugMenuAttribute : Attribute
+        {
+            public string Name { get; }
+
+            public DebugMenuAttribute(string name = null)
+            {
+                Name = name;
+            }
+        }
+
+        [DebugMenu]
+        private void ListTestConfig()
         {
             foreach (var config in TestConfig.AllConfig()) {
                 Debug.Log(config);
             }
         }
 
+        [DebugMenu]
+        private void ListAssemblies()
+        {
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+                Debug.Log(asm.FullName);
+            }
+        }
+
+        private void OnGUI()
+        {
+            var width = 400f;
+            var height = 300f;
+
+            var ratio = Mathf.Min(Screen.width / width, Screen.height / height);
+
+            GUI.matrix = Matrix4x4.TRS(new Vector3(20, 20, 0), Quaternion.identity, new Vector3(ratio, ratio, 1));
+            
+            GUILayout.BeginVertical(GUILayout.Width(100));
+
+            foreach (var action in _list) {
+                if (GUILayout.Button(action.name)) {
+                    action.action();
+                }
+            }
+            
+        }
     }
 }
